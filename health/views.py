2321e8d7b2c9ccabc8f4e516774e27f6f0ab3c78
unknown <omkargattawar6@gmail.com>
2025-04-12 00:51:32 +0530

    codechanges

--- health/views.py
+++ health/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 import datetime
 
-from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.ensemble import RandomForestClassifier
 
 from .forms import DoctorForm
 from .models import *
@@ -185,7 +185,7 @@ def prdict_heart_disease(list_data):
     # age,sex,bp,pulset,height,weight,bmi,spo2,temp,resprate,wbc,plat,hb,hba1c,cho,
     y = df['target']
     X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.8, random_state=0)
-    nn_model = GradientBoostingClassifier(n_estimators=100,learning_rate=1.0,max_depth=1, random_state=0)
+    nn_model = RandomForestClassifier(n_estimators=100,learning_rate=1.0,max_depth=1, random_state=0)
     nn_model.fit(X_train, y_train)
     pred = nn_model.predict([list_data])
     print("Neural Network Accuracy: {:.2f}%".format(nn_model.score(X_test, y_test) * 100))
